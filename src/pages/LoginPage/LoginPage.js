6890369722f8ba9b43047c9d144392afe11e90c2
Abzeede <pvongviseskij@yahoo.com>
2018-01-20 15:59:12 +0700

    add material-ui, formsy-react Do LoginPage Layout

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Button, Input } from 'material-ui'
 
 const LoginPage = () => (
   <div>
-    LOGIN PAGE
+    <h1>DIARY365</h1>
+    <h3>HOW ARE YOU?</h3>
+    <p>I'M <Input name="username" /></p>
+    <h3>WHAT'S YOUR PASSWORD</h3>
+    <p><Input name="password" /></p>
+    <Button>LET ME IN</Button>
   </div>
 )
 
